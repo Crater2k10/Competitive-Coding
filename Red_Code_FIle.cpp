30900b7677a81b88e8eb0f5af51006432301e1d6
Creater2k10 <44505818+Crater2k10@users.noreply.github.com>
2020-05-21 14:13:37 +0530

    Add files via upload

---
+++ Red_Code_FIle.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namspace std;
+
+#define f1(i,a,b)                              	for(int i=a;i<=b;i++)
+#define f2(i,a,b)                              	for(int i=a;i>=b;i--)
+#define tc(test)                               	int test; cin>>test; while(test--)
+#define d_arr(arr,n,type)  						type *arr=new type[n];
+#define mini(a,b)                               a=min(a,b)
+#define maxi(a,b)                               a=max(a,b)
+#define desc                                    greater<int>()
+#define ff              						first
+#define ss              						second
+#define pb                                      push_back
+#define IOS                                     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0) 
+#define ps(x,y)         						fixed<<setprecision(y)<<x
+#define r0                                      return(0)
+#define mod             						1000000007
+#define vi										vector<int>
+#define long long								int
+#define long double								ld
+#define pii 									pair<int,int>  
+#define mii 									map<int,int>                            
+#define	inf										1e18
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>pbds;
+
+void c_p_c()
+{
+	IOS;
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+class Solution{
+	public:
+		void sol{
+			
+		}
+}
+
+int32_t main()
+{
+    c_p_c();
+	Solution a;
+	a.sol();
+	r0;    
+}
